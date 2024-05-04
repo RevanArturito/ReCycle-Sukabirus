fd4c278a2e13defe04ab0e1c808974525e1bb582
Spacyes241 <farrelhaykalindonesia@gmail.com>
2024-05-04 12:52:59 +0700

    Membuat delet

---
+++ DeletMain/Program.cs
@@ -0,0 +1,40 @@
+﻿using Library;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> itemsList = new List<string> { "Profile barang 1", "Profile barang 2", "Profile barang 3", "Profile barang 4 " };
+
+        Console.WriteLine("Daftar item:");
+        for (int i = 0; i < itemsList.Count; i++)
+        {
+            Console.WriteLine((i+1) + ": " + itemsList[i]);
+        }
+
+        Console.WriteLine("Masukkan indeks item yang ingin dihapus:");
+
+        // Membaca input dari pengguna
+        string input = Console.ReadLine();
+
+        // Konversi input menjadi bilangan bulat
+        int index;
+        if (int.TryParse(input, out index))
+        {
+            index = index - 1;
+            // Membuat instance dari kelas DeletAsAdmin dengan list itemsList
+            DeletAsAdmin<string> deletableList = new DeletAsAdmin<string>(itemsList);
+
+            // Memanggil metode DeleteByIndex untuk menghapus item dari list berdasarkan indeks yang dimasukkan oleh pengguna
+            deletableList.DeleteByIndex(index);
+
+            // Tampilkan isi list setelah penghapusan
+            Console.WriteLine("Setelah dihapus:");
+            Console.WriteLine(string.Join(", ", itemsList));
+        }
+        else
+        {
+            Console.WriteLine("Input tidak valid. Harap masukkan bilangan bulat.");
+        }
+    }
+}
---
+++ Library/DeletAsAdmin.cs
@@ -0,0 +1,26 @@
+﻿namespace Library
+{
+    public class DeletAsAdmin<T>
+    {
+        private List<T> items;
+
+        public DeletAsAdmin(List<T> items ) 
+        {
+            this.items = items;
+        }
+
+        public void DeleteByIndex(int index)
+        {
+            if (index >= 0 && index < items.Count)
+            {
+                T deletedItem = items[index];
+                items.RemoveAt(index);
+                Console.WriteLine($"({deletedItem}) telah dihapus.");
+            }
+            else
+            {
+                Console.WriteLine("Indeks tidak valid.");
+            } 
+        }
+    }
+}
